d374a76b6abc7b025740f6e240166b48bd0779e0
wild-hound <wild-hound404@gmail.com>
2020-08-24 02:53:45 +0600

    made blog page which shows all posts by author, date and catagory and split the home page from index.php

--- header.php
+++ header.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+  <title>Document</title>
 </head>
-<body>
-    <header class="site-header">
-      <div class="container">
-        <h1 class="school-logo-text float-left">
-          <a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a>
-        </h1>
-        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
-        <div class="site-header__menu group">
-          <nav class="main-navigation">
-            <ul>
-              <li><a href="<?php echo site_url("/about-page"); ?>">About Us</a></li>
+
+<body <?php body_class(); ?>>
+  <header class="site-header">
+    <div class="container">
+      <h1 class="school-logo-text float-left">
+        <a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a>
+      </h1>
+      <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+      <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+      <div class="site-header__menu group">
+        <nav class="main-navigation">
+          <ul>
+              <li <?php if(is_page("about-page") or wp_get_post_parent_id(0) == 11) echo "class=current-menu-item" ?>><a href="<?php echo site_url("/about-page"); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
@@ -29,6 +31,6 @@
             <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
             <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
           </div>
-        </div>
       </div>
-    </header>
+    </div>
+  </header>
